1c793977916009628a1bc948dfa8dec251aead9d
alexlee <alexlee910116@gmail.com>
2023-02-05 23:42:05 +0900

    オブジェクト

--- index.js
+++ index.js
@@ -12,9 +12,35 @@ const bigUnko = 'He..Hell...Hello World!';
 const inoki = ['いーち', 'にーい', 'さーん', 'ダー〜'];
 
 // ループ文
-let index = 0;
-while(index < inoki.length){
-  console.log(inoki[index]);
-  index++;
+// let index = 0;
+// while(index < inoki.length){
+//   console.log(inoki[index]);
+//   index++;
   
-}
+// }
+
+// if /else 
+// if(inoki.length>5){
+//   console.log('ボンバイエ');
+// } else {
+//   console.log('ボンバ...');
+// }
+
+// 関数
+const test = (arg) => {
+  if(inoki.length> arg){
+    console.log('ボンバイエ');
+  } else {
+    console.log('ボンバ...');
+  }
+};
+
+
+//オブジェクト
+const unko2 = {
+  color: 'pink',
+  size: 'large',
+  purfume: 'mint'
+};
+
+console.log(window.innerHeight);
